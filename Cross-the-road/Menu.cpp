906f8b67b1b824f038e18788f1ba87b824e6e04a
dangcpr2002 <75834491+dangcpr@users.noreply.github.com>
2021-04-30 10:01:58 +0700

    Change menu

--- Cross-the-road/Menu.cpp
+++ Cross-the-road/Menu.cpp
@@ -5,7 +5,6 @@ void CreateConsoleWindow(int pWidth, int pHeight)
 	HWND consoleWindow = GetConsoleWindow();
 	RECT r;
 	HANDLE hConsole;
-
 	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, 240);
 	GetWindowRect(consoleWindow, &r);
@@ -16,16 +15,44 @@ void FixConsoleWindow()
 {
 	HWND consoleWindow = GetConsoleWindow();
 	LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
-
 	style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
 	SetWindowLong(consoleWindow, GWL_STYLE, style);
 }
 
-void GotoXY(int x, int y)
+void GoTo(int x, int y)
 {
 	COORD coord;
 	coord.X = x;
 	coord.Y = y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+
+//Ham do hoa
+void SetColor(int color)
+{
+	HANDLE hConsole;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, color);
+}
+void Text(string name, int color, int x, int y)
+{
+	GoTo(x, y);
+	SetColor(color);
+	cout << name;
+	SetColor(240);
+}
+void Box(int color, int width, int height, int x, int y)
+{
+	SetColor(color);
+	for (int i = 0; i < height; i++)
+	{
+		GoTo(x, y + i);
+		for (int j = 0; j < width; j++)
+		{
+			cout << " ";
+		}
+	}
+	SetColor(240);
+}
+
 
--- Cross-the-road/Menu.h
+++ Cross-the-road/Menu.h
@@ -2,3 +2,6 @@
 #include "Data.h"
 void CreateConsoleWindow(int pWidth, int pHeight);
 void FixConsoleWindow();
+void GoTo(int x, int y);
+//Ham do hoa
+void SetColor(int color);
