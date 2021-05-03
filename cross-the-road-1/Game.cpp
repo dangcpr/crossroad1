5caf65fc1cfd5e384ebe6ac46024213de8cad4b1
dangcpr2002 <75834491+dangcpr@users.noreply.github.com>
2021-05-03 16:06:16 +0700

    change

--- cross-the-road-1/Game.cpp
+++ cross-the-road-1/Game.cpp
@@ -11,9 +11,13 @@ void ControlInGame()
 			PauseGame();
 			char press1;
 			press1 = _getch();
-			if (press1 == 'r')
+			if (press1 == 'r') {
 				ClearScreen(45, 10, X_CENTER + 43, Y_CENTER - 16);
-			GoTo(0, 0);
+				GoTo(0, 0);
+			}
+			else if (press1 == 'e') {
+				ExitGame();
+			}
 		}
 	}
 }
@@ -32,18 +36,19 @@ void PauseGame()
 	Text("*********** PAUSE ************", 117, menu.x, menu.y - 2);
 	Text("   Press 'r' to Resume Game   ", 124, menu.x, menu.y);
 	Text("    Press 's' to Save Game    ", 124, menu.x, menu.y + 1);
+	Text("    Press 'e' to Exit Game    ", 124, menu.x, menu.y + 2);
 }
 void ExitGame()
 {
-	BigText("ThankYou.txt", 71, 40, 15);
-	Sleep(3000);
 	SetColor(255);
+	system("cls");
+	BigText("ThankYou.txt", 71, 40, 15);
 }
 void DrawBoard(int row, int col, int x, int y, int width, int height)
 {
 	for (int i = 0; i < height; i++)
 	{
-		//Sleep(1);
+		//Sleep(0.8);
 		GoTo(x, y + i);
 		cout << "|";
 	}
@@ -51,14 +56,14 @@ void DrawBoard(int row, int col, int x, int y, int width, int height)
 	{
 		for (int j = 0; j <= width; j++)
 		{
-			//Sleep(1);
+			//Sleep(0.8);
 			GoTo(x + j + 1, y + i - 1);
 			cout << "_";
 		}
 	}
 	for (int i = 0; i < height; i++)
 	{
-		//Sleep(1);
+		//Sleep(0.8);
 		GoTo(x + width + 2, y + i);
 		cout << "|";
 	}
